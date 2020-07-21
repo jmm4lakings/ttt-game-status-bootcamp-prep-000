eea6f2b740ce5892780ebd9518c78ca80aacce91
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-21 20:49:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,4 +56,14 @@ end
 
 def winner (board)
   index = []
-  
+  index = won? (board)
+  if index == false 
+    return nil
+  else 
+    if board[index[0]] == "X"
+      return "X"
+    else 
+      return "O"
+    end
+  end
+end 
