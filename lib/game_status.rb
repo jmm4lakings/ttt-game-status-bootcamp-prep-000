03005e8e231eb1bf35cacd7bb0022226c2218e2f
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-21 20:45:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,7 +34,17 @@ WIN_COMBINATIONS = [
   return false
 end 
 
+  def full?(board)
+    board.all? {|index| index == "X"|| index == "O"}
+  end
   
+  def draw?(board)
+    if !won?(board) && full?(board)
+      return true 
+    else 
+      return false 
+    end 
+  end
   
   
   
